442bbba5c378828466aebc0f668bfe556c9f9164
Joao Guilherme da Silva Sousa <joao.silva@lead.fortaleza>
2023-10-26 16:51:53 -0300

    Alteração nas telas

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -10,6 +10,29 @@ export class HomeComponent {
     {
       title: 'Banco de Dados - SQL',
       desc: 'Lorem ipsum dolor, sit amet consectetur adipisicing elit. Unde aspernatur odit quasi maxime labore, rerum earum est magnam commodi doloribus impedit soluta fugiat dolorem consequuntur.'
+    },
+  ]
+
+  categories = [
+    {
+      titleCat: 'Front-End',
+      class: 'fa-solid fa-desktop text-white',
+      descCat: 'Some quick example text to build on the card title and make up the bulk of the cards content.'
+    },
+    {
+      titleCat: 'Back-End',
+      class: 'fa-solid fa-server text-white',
+      descCat: 'Some quick example text to build on the card title and make up the bulk of the cards content.'
+    },
+    {
+      titleCat: 'Banco de Dados',
+      class: 'fa-solid fa-database text-white',
+      descCat: 'Some quick example text to build on the card title and make up the bulk of the cards content.'
+    },
+    {
+      titleCat: 'Web Design',
+      class: 'fa-solid fa-palette text-white',
+      descCat: 'Some quick example text to build on the card title and make up the bulk of the cards content.'
     }
   ]
 }
