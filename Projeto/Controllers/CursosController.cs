edac573d2984469bc1c53422df55b99ee5bfba62
Pedro Yago Lopes Barbosa <pedro.barbosa@lead.fortaleza>
2023-11-21 16:25:48 -0300

    Controllers do curso adicionados

--- Projeto/Controllers/CursosController.cs
+++ Projeto/Controllers/CursosController.cs
@@ -1,3 +1,4 @@
+using System.Linq.Expressions;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using Projeto.Models;
@@ -16,19 +17,91 @@ namespace Projeto.Controllers
     }
 
     [HttpGet]
-
     public async Task<ActionResult<List<Cursos>>> GetAllCursos()
     {
       return Ok(await context.cursos.ToListAsync());
     }
-    [HttpGet("{id}")]
-    public async Task <ActionResult<Cursos>> GetCurso(int id){
-      var curso = await context.cursos.FindAsync(id);
-      if(curso == null) return NotFound("Curso não encontrado");
 
+    [HttpGet("{id}")]
+    public async Task<ActionResult<Cursos>> GetCurso(int id)
+    {
+      var curso = await context.cursos.FindAsync(id);
+      if (curso == null) return NotFound("Curso não encontrado");
       return Ok(curso);
     }
 
-    
+    [HttpGet("intrutor/{id}")]
+    public async Task<ActionResult<IEnumerable<Cursos>>> GetCursoInstrutor(int id)
+    {
+      var cursosDoInstrutor = context.cursos
+     .Where(c => c.InstrutorId == id)
+     .Select(c => new
+     {
+       c.Id_curso,
+       c.Name,
+       c.Data_criacao,
+       c.Disponivel,
+       c.Preco
+       // Adicione mais propriedades do curso conforme necessário
+     }).ToList();
+      if (cursosDoInstrutor == null) return NotFound("Não foi encontrado");
+      return Ok(cursosDoInstrutor);
+    }
+
+    [HttpGet("aluno/{id}")]
+    public async Task<ActionResult<List<Cursos>>> GetAlunoCursos(int id)
+    {
+      var cursosdoAluno = context.cursos.Where(c => c.InstrutorId == id).Select(c => new
+      {
+        c.Name,
+        c.Data_criacao,
+        c.Disponivel,
+        c.Preco
+      }).ToList();
+
+      if (cursosdoAluno == null) return NotFound("Não foram encontrados cursos.");
+
+      return Ok(cursosdoAluno);
+    }
+
+
+    [HttpPost("instrutor")]
+    public async Task<ActionResult<Cursos>> CreateCurso(Cursos novocurso)
+    {
+      if (novocurso == null) return BadRequest("Dados vazios");
+      var cursoexiste = await context.cursos.FindAsync(novocurso.Id_curso);
+      if (cursoexiste != null) return BadRequest("O curso já existe");
+      context.cursos.Add(novocurso);
+      await context.SaveChangesAsync();
+
+      return Ok(novocurso);
+    }
+
+    [HttpPut("{id}")]
+    public async Task<ActionResult<Cursos>> PutCurso(Cursos modifycurso, int id)
+    {
+      if (modifycurso == null) return BadRequest("O curso está nulo");
+      var existingCurso = await context.cursos.FindAsync(modifycurso.Id_curso);
+
+      if (existingCurso == null) return NotFound("Não encontrado");
+      try
+      {
+        existingCurso.Name = modifycurso.Name;
+        existingCurso.Preco = modifycurso.Preco;
+        existingCurso.Data_criacao = modifycurso.Data_criacao;
+        existingCurso.Disponivel = modifycurso.Disponivel;
+
+        context.Update(existingCurso);
+        await context.SaveChangesAsync();
+
+        return Ok(existingCurso);
+      }
+      catch (Exception ex)
+      {
+        return StatusCode(500, $"Erro no servidor: {ex.Message}");
+      }
+
+    }
+
   }
 }
---
+++ Projeto/Controllers/ModulosController.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc;
+
+namespace Projeto.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class ModulosController : ControllerBase
+    {
+
+    }
+}
