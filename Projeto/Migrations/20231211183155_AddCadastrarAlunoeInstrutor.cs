04bfed8a532004ee0cac32369789278b70162c07
João Guilherme <guilhermesousa5423@gmail.com>
2023-12-11 16:28:16 -0300

    Fazendo a integração. Criando lógica de cadastro dos usuários

---
+++ Projeto/Migrations/20231211183155_AddCadastrarAlunoeInstrutor.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Projeto.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCadastrarAlunoeInstrutor : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "instrutors",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "Email",
+                table: "alunos",
+                type: "nvarchar(max)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateTable(
+                name: "user",
+                columns: table => new
+                {
+                    id = table.Column<int>(type: "int", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    senhacriptografada = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    professorornot = table.Column<bool>(type: "bit", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_user", x => x.id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "user");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "instrutors");
+
+            migrationBuilder.DropColumn(
+                name: "Email",
+                table: "alunos");
+        }
+    }
+}
